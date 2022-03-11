6c30d09b44e8dad361ec3f8564c8709915ee222e
Вадим Гончаров <vadim.goncharov.1999@outlook.com>
2022-03-11 17:27:17 +0200

    Добавьте файлы проекта.

---
+++ TCP_Client/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace TCP_Client
+{
+    public class Program
+    {
+        private static void Main()
+        {
+            TcpClient tcpClient = new TcpClient("localhost", 9090);
+
+
+            tcpClient.GetStream().Write(Encoding.ASCII.GetBytes("Hello TCP listener!"));
+
+            tcpClient.Close();
+        }
+    }
+}
---
+++ TCP_Listener/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TCP_Listener
+{
+    public class Programm
+    {
+        static void Main()
+        {
+            TcpListener tcp = new TcpListener(IPAddress.Parse("0.0.0.0"), 9090);
+            tcp.Start(32);
+
+            byte[] buffer = new byte[64];
+
+            using TcpClient tcpClient = tcp.AcceptTcpClient();
+            tcpClient.GetStream().Read(buffer, 0, buffer.Length);
+
+            string data = Encoding.ASCII.GetString(buffer);
+            Console.WriteLine(data);
+
+            tcp.Stop();
+
+        }
+    }
+}
