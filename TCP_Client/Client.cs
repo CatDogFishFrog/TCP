55f7e1f4dc585a0ab74da4d33d2d438e9b9a9e95
Вадим Гончаров <vadim.goncharov.1999@outlook.com>
2022-03-11 18:10:14 +0200

    Ручной воод сообщений, отправляются много собщений. Но соединение разрывается

---
+++ TCP_Client/Client.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace TCP_Client
+{
+    public class Client
+    {
+        private static void Main()
+        {
+            TcpClient tcpClient = new TcpClient("localhost", 9090);
+            StringBuilder buffer = new StringBuilder();
+
+
+            while (true)
+            {
+                Console.WriteLine("Введите свое сообщение:");
+
+                buffer.Clear();
+                buffer.Append(Console.ReadLine());
+
+                tcpClient.GetStream().Write(Encoding.ASCII.GetBytes(buffer.ToString()));
+                
+            }
+
+            tcpClient.Close();
+        }
+    }
+}
---
+++ TCP_Listener/Listener.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TCP_Listener
+{
+    public class Listener
+    {
+        static void Main()
+        {
+            TcpListener tcp = new TcpListener(IPAddress.Parse("0.0.0.0"), 9090);
+            tcp.Start(32);
+
+            byte[] buffer = new byte[512];
+            StringBuilder data = new StringBuilder();
+            Console.WriteLine("Вывод сообщений:");
+
+            while (true)
+            {
+                using TcpClient tcpClient = tcp.AcceptTcpClient();
+                tcpClient.GetStream().Read(buffer, 0, buffer.Length);
+
+                data.Clear();
+                  
+                data.Append(Encoding.ASCII.GetString(buffer));
+
+                Console.WriteLine("Клиент: {0}", data);
+            }
+
+
+            //tcp.Stop();
+
+        }
+    }
+}
